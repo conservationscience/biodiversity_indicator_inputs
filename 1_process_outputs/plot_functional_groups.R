04a583b7d661bce193e9159a124fbd5f78c4ed69
Simonestevo <simonestevenson@gmail.com>
2020-05-27 17:06:57 +1000

    Changed colours so they align with bodymass and added a mean biomass line

--- 1_process_outputs/plot_functional_groups.R
+++ 1_process_outputs/plot_functional_groups.R
@@ -1,33 +1,54 @@
 
 # Git repo "C:\\Users\\ssteven\\Dropbox\\Deakin\\Serengeti-analysis\\model_outputs_to_indicator_inputs\\1_process_outputs"
 
-
+library(tidyverse)
 library(reshape2)
 library(ggplot2)
-library(stringr)
-library(dplyr)
+# library(stringr)
+# library(dplyr)
 
+
+#' TODO: Work out if need to exponentiate autotrophs and heterotrophs
 #' TODO: Add in autotrophs
 #' TODO: Add in bodymass labels to lines
-#' TODO: Add save function
 #' TODO: Generalise object names
-#' TODO: Add a 'mean' line for each group (and test out just including mean with
-#' geom_jitter behind?)
-#' TODO: Add bodymass bins and colour by bin (eg so bodymass bin 75 is the same colour regardless of functional group)
+#' TODO: Make better plot names
+
+
+dev_mode <- FALSE
+
+if(dev_mode == TRUE) {
 
 burnin <- 1*12
 
 simulation_path <- "N:\\Quantitative-Ecology\\Indicators-Project\\Serengeti\\Outputs_from_adaptor_code\\map_of_life\\Test_runs\\ae_BuildModel"
+output_path <- simulation_path
+  
+burnin <- 0
+startimpact <- 12
+endimpact <- 24
+sample_year <- 1
+n <- 1
 
-# burnin <- 1000 * 12
-# 
-# simulation_path <- "N:\\Quantitative-Ecology\\Indicators-Project\\Serengeti\\Outputs_from_adaptor_code\\map_of_life\\Harvesting_carnivores\\203_BuildModel"
+} else {
+
+simulation_path <- "N:\\Quantitative-Ecology\\Indicators-Project\\Serengeti\\Outputs_from_adaptor_code\\map_of_life\\Harvesting_herbivores\\301_BuildModel"
+output_path <- simulation_path
+
+burnin <- 1000 * 12
+startimpact <- 1100 * 12
+endimpact <- 1200 * 12
+sample_year <- 5*12
+n <- 120
+
+}
+
 
 files <- list.files(simulation_path, recursive = FALSE) 
 
 abundance_files <- paste(simulation_path, files[str_detect(files, "biomass.rds")], sep = "\\")
 
-abundance_files <- abundance_files[1:2] # TEMP CODE - subset so you don't read everything in
+#abundance_files <- abundance_files[1:2] # TEMP CODE - subset so you don't read everything in
 
 name_pt_1 <- basename(simulation_path)
 names_pt_2 <- unlist(lapply(abundance_files, basename))
@@ -35,42 +56,67 @@ plot_names <- paste(name_pt_1, names_pt_2, sep = "_")
 
 simulation_abundance_data <- lapply(abundance_files, readRDS)
 
-# Melt into long format
+# Remove burn in and sample every n years to reduce data for plotting
 
-simulation_abundance_data_long <- lapply(simulation_abundance_data, melt)
+subset_simulation_data <- list()
 
-# Replace no value with NA
-
-for (i in seq_along(simulation_abundance_data_long)) {
+for (i in seq_along(simulation_abundance_data)) {
   
-  simulation_abundance_data_long[[i]][simulation_abundance_data_long[[i]] == -9999] <- NA
+  temp <- simulation_abundance_data[[i]][,burnin:ncol(simulation_abundance_data[[i]])]
+  
+  sample <- seq(1, ncol(temp), by = n)
+  
+  subset_simulation_data[[i]] <- temp[,sample]
   
 }
 
+rm(temp)
+
+# Convert back into matrices and melt into long format
+
+subset_simulation_data_long <- lapply(subset_simulation_data, melt)
+
+single <- subset_simulation_data_long[[1]]
+
+# Replace no value with NA
+
+for (i in seq_along(subset_simulation_data_long)) {
+  
+  subset_simulation_data_long[[i]][subset_simulation_data_long[[i]] == -9999] <- NA
+  
+}
+
+head(subset_simulation_data_long[[1]])
+
+# Add grouping variables to prepare data for plotting
+
 plot_data <- list()
 
-for (i in seq_along(simulation_abundance_data_long)) {
+for (i in seq_along(subset_simulation_data_long)) {
   
-names(simulation_abundance_data_long[[i]]) <- c("functional_group", "timestep", "abundance")
-
-simulation_abundance_data_long[[i]]$functional_group <- as.character(simulation_abundance_data_long[[i]]$functional_group)
-
-simulation_abundance_data_long[[i]] <- simulation_abundance_data_long[[i]][!is.na(simulation_abundance_data_long[[i]]$abundance),]
-
-plot_data[[i]] <- simulation_abundance_data_long[[i]] %>%
-                  mutate(group = 
-                         str_sub(simulation_abundance_data_long[[i]]$functional_group, 
-                                 start = 1, end= 2)) %>%
-                  filter(timestep >= burnin) %>%
-                  mutate(functional_group_name = ifelse(group == 10, "herbivorous endotherms",
-                         ifelse(group == 11, "carnivorous  endotherms",
-                         ifelse(group == 12, "omnivorous  endotherms",
-                         ifelse(group == 13, "herbivorous ectotherms", # combine iteroparous and semelparous ectotherms
-                         ifelse(group == 14, "carnivorous ectotherms",
-                         ifelse(group == 15, "omnivorous ectotherms", "NA"))))))) %>%
-                  arrange(functional_group, timestep) %>%
-                  filter(row_number() %% 360 == 0) %>%
-                  mutate(years = seq(1:nrow(.)))
+  names(subset_simulation_data_long[[i]]) <- c("functional_group", "timestep", "abundance")
+  
+  subset_simulation_data_long[[i]]$functional_group <- as.character(subset_simulation_data_long[[i]]$functional_group)
+  
+  subset_simulation_data_long[[i]] <- subset_simulation_data_long[[i]][!is.na(subset_simulation_data_long[[i]]$abundance),]
+  
+  plot_data[[i]] <- subset_simulation_data_long[[i]] %>%
+    mutate(group = 
+             str_sub(subset_simulation_data_long[[i]]$functional_group, 
+                     start = 1, end= 2)) %>%
+    # filter(timestep >= burnin) %>%
+    mutate(functional_group_name = ifelse(group == 10, "herbivorous endotherms",
+                                   ifelse(group == 11, "carnivorous  endotherms",
+                                   ifelse(group == 12, "omnivorous  endotherms",
+                                   ifelse(group == 13, "herbivorous ectotherms", # combine iteroparous and semelparous ectotherms
+                                   ifelse(group == 14, "carnivorous ectotherms",
+                                   ifelse(group == 15, "omnivorous ectotherms", 
+                                   "NA"))))))) %>%
+    arrange(functional_group, timestep) %>%
+    mutate(bodymass_bin = str_sub(functional_group, start= -2)) %>%
+    group_by(group, timestep) %>%
+    mutate(mean_group_abundance = mean(abundance))
+  
 }
 
 dim(plot_data[[1]])
@@ -82,21 +128,34 @@ head(plot_data[[1]])
 abundance_plots <- list()
 
 for (i  in seq_along(plot_data)) {
+  
+  abundance_plots[[i]] <- ggplot() +
+    geom_path(aes(y = abundance, x = timestep, colour = bodymass_bin),
+              data =  plot_data[[i]]) +
+    geom_line(aes(y = mean_group_abundance, x = timestep), 
+              data = plot_data[[i]], size = 1) +
+    theme(legend.position = "none") +
+    geom_vline(xintercept = 10, color = "red") +
+    geom_vline(xintercept = 20 , color = "dark green") +
+    facet_wrap(~ functional_group_name, ncol = 3) +
+    ggtitle(plot_names[[i]])
+  
+  abundance_plots[[i]]
+  
+}
 
-abundance_plots[[i]] <- ggplot() +
-             geom_path(aes(y = abundance, x = timestep, colour = functional_group),
-                  data =  plot_data[[1]]) +
-             theme(legend.position = "none") +
-             geom_vline(xintercept = 1100 *12, color = "red") +
-             geom_vline(xintercept = 1200 * 12, color = "dark green") +
-             facet_wrap(~ functional_group_name, ncol = 3) +
-             ggtitle(plot_names[[i]])
 
-abundance_plots[[i]]
+abundance_plots[[1]]
+abundance_plots[[2]]
+abundance_plots[[3]]
+abundance_plots[[4]]
+abundance_plots[[5]]
+
+for (i in seq_along(abundance_plots)) {
+  
+  ggsave(file.path(output_path, paste(plot_names[[i]],".pdf")), abundance_plots[[i]])
 
 }
 
-abundance_plots[[2]]
-
-ggsave("C:\\Users\\ssteven\\Dropbox\\Deakin\\Serengeti-analysis\\herb303test1.pdf", abundance_plots[[1]],  device = "pdf")
-ggsave("C:\\Users\\ssteven\\Dropbox\\Deakin\\Serengeti-analysis\\herb303test2.4.pdf", abundance_plots[[2]],  device = "pdf")
+# ggsave("C:\\Users\\ssteven\\Dropbox\\Deakin\\Serengeti-analysis\\herb303test1.pdf", abundance_plots[[1]],  device = "pdf")
+# ggsave("C:\\Users\\ssteven\\Dropbox\\Deakin\\Serengeti-analysis\\herb303test2.4.pdf", abundance_plots[[2]],  device = "pdf")
